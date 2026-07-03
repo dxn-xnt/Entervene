e8ace580c7eb9780b4bd89004a98c33ce7a9b771
Sonix-J <limosnerogabriel@gmail.com>
2026-07-03 20:21:47 +0800

    added breadcrumbs for subject view / fixed breadcrumbs for student grade interface

--- frontend/src/pages/student/Grades/subject-grade.tsx
+++ frontend/src/pages/student/Grades/subject-grade.tsx
@@ -9,25 +9,34 @@ type SubjectGradeProps = {
 const SubjectGrade = ({ subject, onBack }: SubjectGradeProps) => {
   return (
     <AppLayout>
-      <header className="px-5 py-5 flex items-center gap-3 border-b border-gray-500">
-        <Breadcrumb>
-          <Breadcrumb.List>
-            <Breadcrumb.Item>
-              <Breadcrumb.Link
-                onClick={onBack}
-                className="text-3xl text-black/50 hover:text-black cursor-pointer"
-              >
-                Grades
-              </Breadcrumb.Link>
-            </Breadcrumb.Item>
-            <Breadcrumb.Separator />
-            <Breadcrumb.Item>
-              <Breadcrumb.Page className="text-2xl">{subject}</Breadcrumb.Page>
-            </Breadcrumb.Item>
-          </Breadcrumb.List>
-        </Breadcrumb>
-      </header>
-      <main className="px-5 py-5"></main>
+      <div className="flex flex-1 flex-col overflow-x-hidden">
+        <div className="@container/main flex flex-1 flex-col">
+          <div className="flex flex-col gap-4 py-4 md:py-5 px-4 md:px-6 pb-6">
+            <header className="flex items-center gap-3 pb-4 -mx-4 md:-mx-6 px-4 md:px-6 border-b border-gray-500">
+              <Breadcrumb>
+                <Breadcrumb.List>
+                  <Breadcrumb.Item>
+                    <Breadcrumb.Link
+                      onClick={onBack}
+                      className="text-2xl md:text-4xl text-black/50 hover:text-black cursor-pointer"
+                    >
+                      Grades
+                    </Breadcrumb.Link>
+                  </Breadcrumb.Item>
+                  <Breadcrumb.Separator />
+                  <Breadcrumb.Item>
+                    <Breadcrumb.Page className="text-2xl">
+                      {subject}
+                    </Breadcrumb.Page>
+                  </Breadcrumb.Item>
+                </Breadcrumb.List>
+              </Breadcrumb>
+            </header>
+
+            <main></main>
+          </div>
+        </div>
+      </div>
     </AppLayout>
   );
 };
